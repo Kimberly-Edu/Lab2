004edae922bcfdddce0059472c085da2c66c061f
Kimberly <KIMBERLYM17.22@ichat.sp.edu.sg>
2023-04-27 15:22:22 +0800

    v1.0

---
+++ bmi.py
@@ -0,0 +1,20 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    # Add code here to calculate BMI
+    bmi = weight/(height**2)
+
+    # Add code here to display calculate BMI
+    print(bmi)
+
+    # Weight Classification
+    if bmi < 18.5:
+        print("Under Weight")
+    elif bmi > 25.0:
+        print("Over Weight")
+    elif (bmi <= 25.0) and (bmi >= 18.5):
+        print("Normal Weight")
+
+calculate_bmi(weight=57, height=1.73)
+
